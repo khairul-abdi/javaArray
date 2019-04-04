e2b99555fde8528c801b9779740527ef03799779
khairul-abdi <kabdi384@gmail.com>
2019-04-04 19:12:37 +0700

    first Commit

---
+++ src/array.java
@@ -0,0 +1,20 @@
+// cara pertama
+//String[] nama;
+
+// cara kedua
+//        String nama[];
+
+// cara ketiga dengan kata kunci new
+//        String[] nama = new String[5];
+
+public class array {
+
+    public static void main(String [] args){
+        // membuat array
+        String[] teman = {"Linda", "Santi", "Susan", "Mila", "Ayu"};
+
+        // mengambil data array
+        System.out.println(teman[2]);
+
+    }
+}
